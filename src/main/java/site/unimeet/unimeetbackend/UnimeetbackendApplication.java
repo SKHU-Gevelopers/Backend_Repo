beef0aad23cf57048217a07cace17d13bc33a7fd
vact19 <chj6703@gmail.com>
2023-05-02 09:32:22 +0900

    init

---
+++ src/main/java/site/unimeet/unimeetbackend/UnimeetbackendApplication.java
@@ -0,0 +1,13 @@
+package site.unimeet.unimeetbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnimeetbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnimeetbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/unimeet/unimeetbackend/UnimeetbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package site.unimeet.unimeetbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnimeetbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
