1051f275f18d5b424ba57a1c1539b034a8cc1450
vact19 <chj6703@gmail.com>
2023-05-05 23:43:31 +0900

    feat: 메일전송 예시 추가

--- src/main/java/site/unimeet/unimeetbackend/api/auth/controller/AuthController.java
+++ src/main/java/site/unimeet/unimeetbackend/api/auth/controller/AuthController.java
@@ -17,11 +17,24 @@ import javax.validation.Valid;
 public class AuthController {
     private final AuthService authService;
 
+
     // 로그인
     @PostMapping("/users/sign-in")
     public SingleRspsTemplate<TokenDto> signIn(@RequestBody @Valid UserSignInDto.Request singInRequest){
         TokenDto tokenDto = authService.signIn(singInRequest.getEmail(), singInRequest.getPassword());
         return new SingleRspsTemplate<>(HttpStatus.OK.value(), tokenDto);
     }
+
+    // 메일 전송 예시
+    // private final JavaMailSender javaMailSender;
+//    @GetMapping("/health")
+//    public String test() {
+//        SimpleMailMessage message = new SimpleMailMessage();
+//        message.setTo("limyeheew@gmail.com");
+//        message.setSubject("WhoAmI의 임시 비밀번호를 발급해 드려요");
+//        message.setText("받아라");
+//        javaMailSender.send(message);
+//        return "hello";
+//    }
 
 }
