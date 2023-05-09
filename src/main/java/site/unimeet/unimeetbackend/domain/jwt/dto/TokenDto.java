07c74d8c7955ca139a867f0f82b88421d9a0e41d
vact19 <chj6703@gmail.com>
2023-05-09 12:37:11 +0900

    rename: expire -> expiration

--- src/main/java/site/unimeet/unimeetbackend/domain/jwt/dto/TokenDto.java
+++ src/main/java/site/unimeet/unimeetbackend/domain/jwt/dto/TokenDto.java
@@ -17,11 +17,11 @@ public class TokenDto {
     private String accessToken;
 
     @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss", timezone="Asia/Seoul")
-    private Date accessTokenExpireTime;
+    private Date accessTokenExpirationTime;
 
     private String refreshToken;
 
     @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss", timezone="Asia/Seoul")
-    private Date refreshTokenExpireTime;
+    private Date refreshTokenExpirationTime;
 
 }
