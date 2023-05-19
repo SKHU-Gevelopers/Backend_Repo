456ab01d27f25cdba88b72d35a1a221722d9422c
vact19 <chj6703@gmail.com>
2023-05-20 00:46:31 +0900

    feat: StudentEmailArgResolver.java

--- src/main/java/site/unimeet/unimeetbackend/global/config/WebConfig.java
+++ src/main/java/site/unimeet/unimeetbackend/global/config/WebConfig.java
@@ -4,6 +4,7 @@ package site.unimeet.unimeetbackend.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import site.unimeet.unimeetbackend.global.converter.CategoryConverter;
@@ -11,12 +12,19 @@ import site.unimeet.unimeetbackend.global.converter.DepartmentConverter;
 import site.unimeet.unimeetbackend.global.converter.GenderConverter;
 import site.unimeet.unimeetbackend.global.converter.MajorConverter;
 import site.unimeet.unimeetbackend.global.interceptor.AuthenticationInterceptor;
+import site.unimeet.unimeetbackend.global.resolver.StudentEmailArgResolver;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    // Converters
     private final CategoryConverter categoryConverter; private final DepartmentConverter departmentConverter;
     private final MajorConverter majorConverter; private final GenderConverter genderConverter;
+    // Resolvers
+    private final StudentEmailArgResolver studentEmailArgResolver;
+
     private final AuthenticationInterceptor authenticationInterceptor;
     @Override
     public void addFormatters(FormatterRegistry registry) {
@@ -31,8 +39,13 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(authenticationInterceptor)
                 .order(1)   // 인증 인터셉터를 첫 번째로 수행
                 .addPathPatterns("/**")     // 이 경로를 대상으로 동작
-                .excludePathPatterns("/auth/**", "/users/sign-up")  // 이 경로는 검사 제외
+                .excludePathPatterns("/auth/**", "/users/sign-up", "/test")  // 이 경로는 검사 제외
         ;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(studentEmailArgResolver);
     }
 }
 
