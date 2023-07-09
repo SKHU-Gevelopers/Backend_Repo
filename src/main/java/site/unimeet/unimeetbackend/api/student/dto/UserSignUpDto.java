59c9b7aaefabb3ea7f178dc1d8ba83722532afce
vact19 <chj6703@gmail.com>
2023-07-09 11:44:17 +0900

    update: 회원가입 age 제외

--- src/main/java/site/unimeet/unimeetbackend/api/student/dto/UserSignUpDto.java
+++ src/main/java/site/unimeet/unimeetbackend/api/student/dto/UserSignUpDto.java
@@ -21,11 +21,6 @@ public class UserSignUpDto {
         private String name;
         @Length(min = 2, max = 10, message = "이름은 2~10자 사이여야 합니다")
         private String nickname;
-        @Min(value = 15, message = "나이는 15살 이상이어야 합니다")
-        @Max(value = 100, message = "나이는 100살 이하여야 합니다")
-        // Todo Size는 Collection, Map, CharSequence, Array에만  적용 가능한지 테스트
-//        @Size(min = 1, max = 3, message = "나이는 1~3자리여야 합니다")
-        private byte age;
         @Email(message = "Email 형식이어야 합니다")
         private String email;
         @Length(min = 4, max = 20, message = "비밀번호는 4~20자 사이여야 합니다")
