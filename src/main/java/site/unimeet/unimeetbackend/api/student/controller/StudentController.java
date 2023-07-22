48c16306d6e040d454a21988377da7b709b33939
vact19 <chj6703@gmail.com>
2023-07-22 15:36:41 +0900

    Rename: api 패키지 구조 변경
    컨트롤러와 dto를 분할하기 위해
    component 패키지 하위에 별도의 구성요소 패키지를 만들어 domain 패키지와 구조를 같게 함

--- src/main/java/site/unimeet/unimeetbackend/api/student/controller/StudentController.java
+++ src/main/java/site/unimeet/unimeetbackend/api/student/controller/StudentController.java
@@ -1,14 +1,15 @@
 package site.unimeet.unimeetbackend.api.student.controller;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import site.unimeet.unimeetbackend.api.common.RspsTemplate;
-import site.unimeet.unimeetbackend.api.student.dto.*;
+import site.unimeet.unimeetbackend.api.student.dto.EditMyPageDto;
+import site.unimeet.unimeetbackend.api.student.dto.MyPageDto;
+import site.unimeet.unimeetbackend.api.student.dto.PublicMyPageDto;
+import site.unimeet.unimeetbackend.api.student.dto.UserSignUpDto;
 import site.unimeet.unimeetbackend.domain.student.Student;
 import site.unimeet.unimeetbackend.domain.student.StudentService;
 import site.unimeet.unimeetbackend.domain.student.component.guestbook.GuestBookService;
@@ -17,11 +18,10 @@ import site.unimeet.unimeetbackend.global.resolver.StudentEmail;
 import site.unimeet.unimeetbackend.util.S3Service;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
 
 
 @RequiredArgsConstructor
-@RestController
+@RestController("/users")
 public class StudentController {
     private final StudentService studentService;
     private final GuestBookService guestBookService;
@@ -30,7 +30,7 @@ public class StudentController {
     private final S3Service s3Service;
 
     // 회원가입
-    @PostMapping("/users/sign-up")
+    @PostMapping("/sign-up")
     public ResponseEntity<RspsTemplate<String>> handleSignUp(@RequestBody @Valid UserSignUpDto.Request signUpRequest){
         Student student = studentService.signUp(signUpRequest.toEntity(passwordEncoder), signUpRequest.getEmailVrfCode());
 
@@ -40,7 +40,7 @@ public class StudentController {
     }
 
     // 마이페이지 수정
-    @PostMapping("/users/my-page")
+    @PostMapping("/my-page")
     public ResponseEntity<?> handleEditMyPage(@ModelAttribute @Valid EditMyPageDto.Request editMyPageRequest
                                 , @StudentEmail String email) {
         String uploadedFileUrl = s3Service.upload(editMyPageRequest.getProfileImg(), S3Config.BUCKETNAME_SUFFIX_PROFILE_IMG);
@@ -50,7 +50,7 @@ public class StudentController {
     }
 
     // 마이페이지 수정창 조회
-    @GetMapping("/users/my-page")
+    @GetMapping("/my-page")
     public RspsTemplate<MyPageDto.Response> handleGetPrivateMyPage(@StudentEmail String email) {
         MyPageDto.Response rspsDto = studentService.getMyPage(email);
 
@@ -58,25 +58,8 @@ public class StudentController {
         return rspsTemplate;
     }
 
-    // 방명록 작성
-    @PostMapping("/users/{userId}/guestbooks")
-    public ResponseEntity<?> handleWriteGuestbook(@StudentEmail String writerEmail,
-                                            @PathVariable("userId") Long targetUserId, @RequestBody @Valid WriteGuestBookDto reqDto) {
-        guestBookService.write(targetUserId, reqDto.getContent(), writerEmail);
-
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
-
-    // DTO 크기가 매우 작아 따로 관리할 필요가 없을 것으로 판단 Inner Class 사용
-    @Getter
-    @NoArgsConstructor
-    private static class WriteGuestBookDto {
-        @NotBlank
-        private String content;
-    }
-
      // 공개 마이페이지 조회
-    @GetMapping("/users/{userId}/my-page")
+    @GetMapping("/{userId}/my-page")
     public RspsTemplate<PublicMyPageDto.Res> handleGetMyPage(@PathVariable Long userId) {
         PublicMyPageDto.Res rspsDto = studentService.getPublicMyPage(userId);
 
