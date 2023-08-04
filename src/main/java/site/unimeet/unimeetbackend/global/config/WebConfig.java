8224dffe370901a1f7a96fb7351e01065de60f7d
vact19 <chj6703@gmail.com>
2023-08-04 13:20:24 +0900

    Fix: prefight 요청 허용

--- src/main/java/site/unimeet/unimeetbackend/global/config/WebConfig.java
+++ src/main/java/site/unimeet/unimeetbackend/global/config/WebConfig.java
@@ -3,16 +3,11 @@ package site.unimeet.unimeetbackend.global.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.format.FormatterRegistry;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import site.unimeet.unimeetbackend.global.converter.CategoryConverter;
-import site.unimeet.unimeetbackend.global.converter.DepartmentConverter;
-import site.unimeet.unimeetbackend.global.converter.GenderConverter;
-import site.unimeet.unimeetbackend.global.converter.MajorConverter;
 import site.unimeet.unimeetbackend.global.interceptor.AuthenticationInterceptor;
 import site.unimeet.unimeetbackend.global.resolver.StudentEmailArgResolver;
 
@@ -21,20 +16,11 @@ import java.util.List;
 @RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    // Converters
-    private final CategoryConverter categoryConverter; private final DepartmentConverter departmentConverter;
-    private final MajorConverter majorConverter; private final GenderConverter genderConverter;
     // Resolvers
     private final StudentEmailArgResolver studentEmailArgResolver;
 
     private final AuthenticationInterceptor authenticationInterceptor;
-    @Override
-    public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(categoryConverter);
-        registry.addConverter(departmentConverter);
-        registry.addConverter(majorConverter);
-        registry.addConverter(genderConverter);
-    }
+
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
