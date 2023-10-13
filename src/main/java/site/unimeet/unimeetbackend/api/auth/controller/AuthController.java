a621bedb046375da1192da5c2d0c43925ed4d44e
vact19 <chj6703@gmail.com>
2023-10-13 11:12:18 +0900

    테스트용 임시 - 카카오 인증 구현중

--- src/main/java/site/unimeet/unimeetbackend/api/auth/controller/AuthController.java
+++ src/main/java/site/unimeet/unimeetbackend/api/auth/controller/AuthController.java
@@ -5,7 +5,6 @@ import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,11 +32,6 @@ public class AuthController {
     private final StudentService studentService;
     private final EmailService emailService;
 
-    // 인증 테스트
-    @GetMapping("/")
-    public ResTemplate<String> test() {
-        return new ResTemplate<>(HttpStatus.OK, "인증 테스트");
-    }
 
     // 로그인
     @PostMapping("/auth/sign-in")
