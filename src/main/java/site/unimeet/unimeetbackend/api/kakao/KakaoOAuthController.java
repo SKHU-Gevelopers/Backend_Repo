1f103d7334cb73eb87ef51c1b7860d68f738b5d2
vact19 <chj6703@gmail.com>
2023-10-15 19:46:09 +0900

    Refactor. Oauth Controller에서 사용하지 않는 의존성 제거

--- src/main/java/site/unimeet/unimeetbackend/api/kakao/KakaoOAuthController.java
+++ src/main/java/site/unimeet/unimeetbackend/api/kakao/KakaoOAuthController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import site.unimeet.unimeetbackend.domain.jwt.service.TokenManager;
 
 import java.util.Map;
 
@@ -25,11 +24,7 @@ public class KakaoOAuthController {
     private final String GRANT_TYPE = "authorization_code";
     @Value("${kakao.private}")
     private String clientId;
-    private final TokenManager tokenManager;
 
-    public KakaoOAuthController(TokenManager tokenManager) {
-        this.tokenManager = tokenManager;
-    }
 
     @GetMapping("/auth/kakao/callback")
     public KakaoToken kakaoCallback(
