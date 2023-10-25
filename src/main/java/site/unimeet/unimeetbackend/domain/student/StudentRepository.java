10d3358a6445360f072b1cbb4eff7051b01a0e86
vact19 <chj6703@gmail.com>
2023-10-25 11:26:53 +0900

    Fix. 전공정보가 없는 사용자를 Inner Join으로 찾지 못하는 문제 해결
    Left Join 사용

--- src/main/java/site/unimeet/unimeetbackend/domain/student/StudentRepository.java
+++ src/main/java/site/unimeet/unimeetbackend/domain/student/StudentRepository.java
@@ -8,7 +8,9 @@ import java.util.Optional;
 public interface StudentRepository extends JpaRepository <Student,Long> {
     Optional<Student> findByEmail(String email);
 
-    @Query("Select distinct s From Student s Join Fetch s.majors Where s.id = :id")
+    @Query("Select distinct s From Student s" +
+            " Left Join Fetch s.majors" +
+            " Where s.id = :id")
     Student findByIdFetchMajors(long id);
 
     boolean existsByKakaoId(String kakaoId);
